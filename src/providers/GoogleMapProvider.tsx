33474e66414356254f4959b58af740cf7dd83761
Flemming Eriksson <fleeri91@gmail.com>
2025-07-28 14:27:22 +0200

    google map provider

---
+++ src/providers/GoogleMapProvider.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { APIProvider } from '@vis.gl/react-google-maps'
+
+export default function GoogleMapProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof APIProvider>) {
+  return (
+    <APIProvider apiKey={props.apiKey} libraries={['geometry']}>
+      {children}
+    </APIProvider>
+  )
+}
