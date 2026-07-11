c43277b9210a09b73892b12faec44285485631f5
Flemming Eriksson <fleeri91@gmail.com>
2026-07-11 22:29:35 +0200

    format

--- providers/query-provider.tsx
+++ providers/query-provider.tsx
@@ -1,20 +1,20 @@
-"use client";
+'use client'
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import React from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React from 'react'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export default function QueryProvider({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  );
+  )
 }
